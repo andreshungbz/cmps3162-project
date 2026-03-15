2c40a33c2a7d9df41220181a598df7ebe58dc0cb
Andres Hung <2018118240@ub.edu.bz>
2026-03-14 21:30:43 -0600

    misc: Add test authentication tokens

--- migrations/000008_token_table.up.sql
+++ migrations/000008_token_table.up.sql
@@ -7,3 +7,9 @@ CREATE TABLE IF NOT EXISTS token (
     expiry TIMESTAMP(0) WITH TIME ZONE NOT NULL,
     scope TEXT NOT NULL
 );
+
+INSERT INTO token (hash, person_id, expiry, scope)
+VALUES
+    (digest('NTH5BYOPVS6WJ2XOK27W7Q6ZUH', 'sha256'), 1, '2126-05-15', 'authentication'),
+    (digest('UZPHDJXKVJORSLLQD7AH3QDCU3', 'sha256'), 2, '2126-05-15', 'authentication'),
+    (digest('UKAKGCKBNSDEJPYBN7S6LFZI3W', 'sha256'), 3, '2126-05-15', 'authentication');
