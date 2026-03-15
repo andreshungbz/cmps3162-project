ca229afef0546d6ba0c2cb10e4ed28846c019d08
Andres Hung <2018118240@ub.edu.bz>
2026-03-15 16:12:07 -0600

    feat: Add CORS Preflight example

---
+++ cmd/examples/cors/preflight/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"flag"
+	"html/template"
+	"log"
+	"net/http"
+
+	"github.com/andreshungbz/cmps3162-project/ui"
+)
+
+func main() {
+	addr := flag.String("addr", ":9000", "Server address")
+	flag.Parse()
+
+	// parse template from embedded filesystem
+	tmpl := template.Must(template.ParseFS(
+		ui.FS,
+		"html/examples/cors/preflight/index.tmpl",
+	))
+
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		err := tmpl.Execute(w, nil)
+		if err != nil {
+			http.Error(w, "Template error", http.StatusInternalServerError)
+			log.Println(err)
+		}
+	})
+
+	log.Printf("Starting Preflight CORS example server on %s", *addr)
+	log.Fatal(http.ListenAndServe(*addr, mux))
+}
---
+++ ui/fs.go
@@ -0,0 +1,6 @@
+package ui
+
+import "embed"
+
+//go:embed html static
+var FS embed.FS
